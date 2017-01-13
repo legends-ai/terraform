cc008312b614723523496edc68b6bfcfa153fe56
Ian Macalinao <me@ian.pw>
2017-01-12 23:55:33 -0600

    Create basic terraform config

---
+++ legends.tf
@@ -0,0 +1,16 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "never_let_it_die" {
+  ami = "ami-0d729a60"
+  instance_type = "t2.micro"
+}
