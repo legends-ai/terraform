10daf010165846d76aa0186cf828332a98630654
Ian Macalinao <me@ian.pw>
2017-01-13 00:03:38 -0600

    use aws credentials file

--- legends.tf
+++ legends.tf
@@ -1,13 +1,10 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
+variable "aws_region" {
   default = "us-east-1"
 }
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  region     = "${var.aws_region}"
+  profile    = "asuna"
 }
 
 resource "aws_instance" "never_let_it_die" {
