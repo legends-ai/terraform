21ba97be25685d95e8272c15355ec6d1b417fd24
Ian Macalinao <me@ian.pw>
2017-01-13 20:50:16 -0600

    remove consul

--- main.tf
+++ main.tf
@@ -12,14 +12,3 @@ provider "aws" {
 resource "aws_ecs_cluster" "asuna" {
   name = "asuna"
 }
-
-// Consul for service discovery
-module "consul" {
-  source = "github.com/hashicorp/consul/terraform/aws"
-
-  instance_type = "t2.micro"  // ayylmao this is dev
-  key_name = "asunachan"
-  key_path = "~/asuna.pem"
-  region = "${var.aws_region}"
-  servers = "1"
-}
