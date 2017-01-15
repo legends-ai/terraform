2ba570e9f64a9ed5b85fdd70ac0474669ff624a8
Ian Macalinao <me@ian.pw>
2017-01-15 01:50:02 -0600

    kill switch formatting

--- dev/scripts/kill_nova.sh
+++ dev/scripts/kill_nova.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
-aws ecs describe-tasks --tasks \
-    $(aws ecs list-tasks --region=us-east-1 \
-          --cluster=dev | jq '.taskArns | join(" ")' \
-             | sed -e 's/^"//' -e 's/"$//' \
-    ) --region=us-east-1 --cluster=dev | jq '.tasks[] | select(.taskDefinitionArn | contains("nova"))'
+aws ecs stop-task --region=us-east-1 --cluster=dev \
+  $(aws ecs describe-tasks --region=us-east-1 --cluster=dev --tasks \
+    $(aws ecs list-tasks --region=us-east-1 --cluster=dev \
+      | jq '.taskArns | join(" ")' \
+      | sed -e 's/^"//' -e 's/"$//') \
+    | jq '.tasks[] | select(.taskDefinitionArn | contains("nova"))'
+)
