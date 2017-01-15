20e6494757de9e5ca254a540615ffe30d385abf5
Ian Macalinao <me@ian.pw>
2017-01-15 16:07:24 -0600

    nova kill switch

--- dev/scripts/kill_nova.sh
+++ dev/scripts/kill_nova.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
-aws ecs stop-task --region=us-east-1 --cluster=dev \
-  $(aws ecs describe-tasks --region=us-east-1 --cluster=dev --tasks \
-    $(aws ecs list-tasks --region=us-east-1 --cluster=dev \
-      | jq '.taskArns | join(" ")' \
-      | sed -e 's/^"//' -e 's/"$//') \
-    | jq '.tasks[] | select(.taskDefinitionArn | contains("nova"))'
-)
+TASKS=`aws ecs list-tasks --region=us-east-1 --cluster=dev | jq '.taskArns | join(" ")' | sed -e 's/^"//' -e 's/"$//'`
+echo $TASKS
+
+TASK_INFO=`aws ecs describe-tasks --region=us-east-1 --cluster=dev --tasks $(echo $TASKS) | jq '.tasks[] | select(.taskDefinitionArn | contains("nova"))'`
+echo $TASK_INFO
+
+TASK_ID=`echo $TASK_INFO | jq .taskArn`
+echo $TASK_ID
+
+aws ecs stop-task --region=us-east-1 --cluster=dev --reason "Kill switch" --task $TASK_ID
