0ce6cc5d43a7caeba6f130e7340f96d1f212996f
Ian Macalinao <me@ian.pw>
2017-01-15 19:59:43 -0600

    cidr block separate

--- dev/main.tf
+++ dev/main.tf
@@ -41,7 +41,7 @@ resource "aws_subnet" "main" {
 
 resource "aws_subnet" "main_2" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.availability_zone_2}"
 }
 
