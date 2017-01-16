8c1422f0fb1c11e6ccbab819e19f948107866b59
Pradyuman Vig <pradyumanvig@outlook.com>
2017-01-15 22:55:48 -0600

    add ssh ingress

--- dev/main.tf
+++ dev/main.tf
@@ -54,6 +54,14 @@ resource "aws_security_group" "ecs" {
   description = "Allows all traffic"
   vpc_id      = "${aws_vpc.main.id}"
 
+  // ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   // Legends.AI
   ingress {
     from_port   = 7448
