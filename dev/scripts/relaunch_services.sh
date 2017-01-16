d65f6e800431f7295ef7d91f62deeaf8675d0167
Ian Macalinao <me@ian.pw>
2017-01-15 19:13:35 -0600

    update relaunch script

--- dev/scripts/relaunch_services.sh
+++ dev/scripts/relaunch_services.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Relaunch all of our services.
-tf taint aws_ecs_service.alexandria
-tf taint aws_ecs_service.charon
-tf taint aws_ecs_service.helios
-tf taint aws_ecs_service.legends-ai
-tf taint aws_ecs_service.lucinda
-tf taint aws_ecs_service.luna
-tf taint aws_ecs_service.nova
-tf taint aws_ecs_service.vulgate
+terraform taint aws_ecs_service.alexandria
+terraform taint aws_ecs_service.charon
+terraform taint aws_ecs_service.helios
+terraform taint aws_ecs_service.legends-ai
+terraform taint aws_ecs_service.lucinda
+terraform taint aws_ecs_service.luna
+terraform taint aws_ecs_service.nova
+terraform taint aws_ecs_service.vulgate
