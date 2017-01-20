c23784eab62c6d6643cb957757dd282fcc844485
Ian Macalinao <me@ian.pw>
2017-01-19 22:18:21 -0600

    fix relaunch script

--- dev/scripts/relaunch_services.sh
+++ dev/scripts/relaunch_services.sh
@@ -3,6 +3,7 @@
 # Relaunch all of our services.
 terraform taint aws_ecs_service.alexandria
 terraform taint aws_ecs_service.charon
+terraform taint aws_ecs_service.dd-agent
 terraform taint aws_ecs_service.helios
 terraform taint aws_ecs_service.legends-ai
 terraform taint aws_ecs_service.lucinda
