8837d93052913e6fee3f67f9afb3787f4deee9dc
Pradyuman Vig <pradyumanvig@outlook.com>
2017-02-18 01:59:19 -0600

    update cassandra for port 22

--- dev/cassandra.tf
+++ dev/cassandra.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "cassandra" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
